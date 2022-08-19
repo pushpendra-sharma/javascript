6471c31adb40eb3171af1c44a347ba09d152975f
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-08-20 02:25:23 +0530

    feat: added factorial fn

---
+++ factorial.js
@@ -0,0 +1,20 @@
+// using recursion O(n) ,recursion = slow
+function factorialByRecursion(n) {
+  if (n < 2) return 1;
+  else return n * factorialByRecursion(n - 1);
+}
+
+// O(n)
+function factorial(n) {
+  if (typeof n === 'number' && n >= 0) {
+    let fact = 1;
+    while (n > 1) {
+      fact = fact * n;
+      n = n - 1;
+    }
+    return fact;
+  } else return 'Enter valid number.';
+}
+
+console.log(factorial(1));
+console.log(factorialByRecursion(6));
