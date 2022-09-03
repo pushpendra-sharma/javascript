536138713639eea0f0ad2e11b28c68ddcb38222a
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-09-03 07:47:22 +0530

    added sliding window pattern

--- Algorithms/slidingWindow.js
+++ Algorithms/slidingWindow.js
@@ -1,6 +1,8 @@
 // ===============  Sliding Window ========================
+// Implementation in string?
 
 // Time Complexity - O(n)  Space Complexity -
+// Fixed window
 function maxSubarraySum(arr, num) {
   let maxSum = 0;
   let tempSum = 0;
@@ -19,3 +21,59 @@ function maxSubarraySum(arr, num) {
 }
 
 console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+// ====================== Variable window ===============================
+// Constraints- Time complexity O(n)   Space complexity O (1)
+function minSubArrayLen(nums, targetSum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while (start < nums.length) {
+    // if current windowSum < targetSum then move the window to right
+    if (total < targetSum && end < nums.length) {
+      total += nums[end];
+      end++;
+    }
+    // if current windowSum >= targetSum given then we can shrink the window
+    else if (total >= targetSum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
+
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest subarray
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+
+// ========================================================================
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
