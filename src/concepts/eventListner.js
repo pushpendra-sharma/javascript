edeedf896eb3624cf40a4504b751c290d45efcc9
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-04-06 22:00:09 +0530

    feat: added event bubbling & capturing example

--- src/concepts/eventListner.js
+++ src/concepts/eventListner.js
@@ -21,3 +21,56 @@ function attatchEventListner() {
 }
 
 attatchEventListner();
+
+const grandParentDiv = document.getElementById('grandparent');
+const parentDiv = document.getElementById('parent');
+const childDiv = document.getElementById('child');
+
+function childClickHandler() {
+  console.log('Child clicked');
+}
+
+function parentClickHandler() {
+  console.log('Parent clicked');
+}
+
+function grandParentClickHandler() {
+  console.log('Grandparent clicked');
+}
+
+/** In case of mix, first capturing then bubbling */
+
+/**
+ *   Event Bubbling, passing the third argument as false(default)
+ *
+ * - Clicking on the child element prints
+ *    Child clicked
+ *    Parent clicked
+ *    Grandparent clicked
+ *
+ * - Clicking on the parent element prints
+ *    Parent clicked
+ *    Grandparent clicked
+ *
+ */
+grandParentDiv.addEventListener('click', grandParentClickHandler);
+parentDiv.addEventListener('click', parentClickHandler);
+childDiv.addEventListener('click', childClickHandler);
+
+/**
+ *    Event capturing, passing the third argument as true
+ *
+ * - Clicking on the child element prints
+ *    Grandparent clicked
+ *    Parent clicked
+ *    Child clicked
+ *
+ *
+ * - Clicking on the parent element prints
+ *    Grandparent clicked
+ *    Parent clicked
+ *
+ */
+grandParentDiv.addEventListener('click', grandParentClickHandler, true);
+parentDiv.addEventListener('click', parentClickHandler, true);
+childDiv.addEventListener('click', childClickHandler, true);
