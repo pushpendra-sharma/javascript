3ae1592c928eb7ac1405ac89baf33d4a4257ff31
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2024-10-20 00:38:33 +0530

    understand optimized bubble sort

--- src/algorithms/sorting/bubbleSort.js
+++ src/algorithms/sorting/bubbleSort.js
@@ -7,20 +7,45 @@ function swap(arr, idx1, idx2) {
  * Space complexity O(1)
  */
 function bubbleSort(array) {
+  // let iterations = 0;
+
   for (let i = array.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
       console.log(array[j], array[j + 1]);
+      // iterations++;
 
       if (array[j] > array[j + 1]) {
         swap(array, j, j + 1);
       }
     }
   }
+
+  // console.log('iterations', iterations);
+  // Iterations are 36, 55
+
   return array;
 }
 
-console.log(bubbleSort([5, 7, 2, 4, 0, 1, 8, 9, 4]));
-console.log(bubbleSort([8, 5, 2, 6, 9, 0, 1, 6, 4, 10, 13]));
+/** This implementation does more iterations, i.e less optimized */
+function bubbleSort2(array) {
+  // let iterations = 0;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      // iterations++;
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+  // console.log('iterations', iterations);
+  // Iterations are 44, 65;
+
+  return array;
+}
+
+console.log(bubbleSort2([5, 7, 2, 4, 0, 1, 8, 9, 4]));
+console.log(bubbleSort2([8, 5, 2, 6, 9, 0, 1, 6, 4, 10, 13]));
 console.log(bubbleSort([]));
 
 // =========== Optimized BubbleSort with noSwaps ==========
