6248273208a5b4232588448317320c168dc2127f
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2025-03-15 22:26:20 +0530

    feat: add filter polyfill

---
+++ src/concepts/polyfills/arrayPolyfills.js
@@ -0,0 +1,43 @@
+const array = ['Bot', 'Angle', 'Elon'];
+
+array[5] = 'Musk';
+// const array = ['Bot', 'Angle', 'Elon'];
+
+if (!Array.prototype.myFilter) {
+  Array.prototype.myFilter = function (callback, thisArg) {
+    if (typeof callback !== 'function') {
+      throw new TypeError(`${callback} is not a function`);
+    }
+
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+      console.log(i);
+      console.log(this);
+
+      if (i in this) {
+        // Check if the index exists to handle sparse arrays
+        /**
+         Uses .call(thisArg, ...), explicitly setting the this value for the callback.
+        Ensures the callback executes with the correct context (thisArg).
+         */
+        if (callback.call(thisArg, this[i], i, this)) {
+          result.push(this[i]);
+        }
+      }
+    }
+    return result;
+  };
+}
+
+const filteredOutputUsingBuiltInFilter = array.filter(item => item.length > 3);
+const filteredOutputUsingPolyfillFilter = array.myFilter(
+  item => item.length > 3
+);
+
+console.log(filteredOutputUsingBuiltInFilter);
+console.log(filteredOutputUsingPolyfillFilter);
+
+const arr = [1, , 3, 4]; // Sparse array (index 1 is missing)
+const filtered = arr.myFilter(num => num > 2);
+
+console.log(filtered);
